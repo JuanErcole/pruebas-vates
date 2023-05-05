4ac0227928605e242e4a3e7682520e6e7a27c63f
Juan Ercole <juan.m.ercole@gmail.com>
2023-05-05 15:12:49 -0300

    fix

--- src/pages/ReactPage/ReactPage.jsx
+++ src/pages/ReactPage/ReactPage.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import DrawerCustom from '../components/DrawerCustom'
 
 export const ReactPage = () => {
   return (
     <>
-      <DrawerCustom />
       <div>ReactPage</div>
     </>
   )
--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { HomePage } from '../pages/HomePage/HomePage'
-import { ReactPage } from '../pages/ReactPage'
-import { PythonPage } from '../pages/PythonPage'
+import { ReactPage } from '../pages/ReactPage/ReactPage'
+import { PythonPage } from '../pages/PythonPage/PythonPage'
 import { Navbar } from '../components/Navbar'
 import { PagesContainer } from '../components/PagesContainer'
 
