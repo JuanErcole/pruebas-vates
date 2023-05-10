88a9398fd92db4c08be6fec9483483cf76fb29ce
Juan Ercole <juan.m.ercole@gmail.com>
2023-05-10 19:54:34 -0300

    add components in pruebas page

--- src/pages/Pruebas/Pruebas.jsx
+++ src/pages/Pruebas/Pruebas.jsx
@@ -1,8 +1,14 @@
+import { Grid } from '@mui/material'
 import React from 'react'
+import { Prueba1 } from './Prueba1'
+import { Prueba2 } from './Prueba2'
 
 export const Pruebas = () => {
 
   return (
-    <div>Pruebas</div>
+    <Grid container spacing={1}>
+      <Prueba1 />
+      <Prueba2 />
+    </Grid>
   )
 }
